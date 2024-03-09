4c500ab942417e764f28dc465704d661a12e7908
summrsvfx <162812942+summrsvfx@users.noreply.github.com>
2024-03-09 09:14:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening to Telfaz Roleplay","Watching Telfaz Clips","Demoting Administrators"];
 
 
 let currentIndex = 0;
